5c3d44d433d6f60deab0ef36c57051f17d39c191
Kijinosu <kijinosu@proton.me>
2026-07-07 15:14:55 +0900

    Version 0.0.3.1

--- docs/devnotes/index.rst
+++ docs/devnotes/index.rst
@@ -20,3 +20,11 @@ These are Jupyter notebooks that were generated while developing the `openczjp` 
 
 .. _Notebook 1: Notebook-1-municipal-codes.html
 
+`Notebook 2`_: Developing estat.municipal_code_download_csv()
+
+.. _Notebook 2: Notebook-2-muni-code-download-func.html
+
+`Notebook 3`_: Developing estat.municipal_code_download_csv() using estatjp v0.1.5
+
+.. _Notebook 3: Notebook-3-municipal-code-download.html
+
--- src/openczjp/estat.py
+++ src/openczjp/estat.py
@@ -22,14 +22,82 @@ See, for example:
 .. _Municipal codes in Japanese: https://www.e-stat.go.jp/dbview?sid=0000020101
 
 """
-API_URLS: Final = {
-"Population_en":"http://api.e-stat.go.jp/rest/3.0/app/getSimpleStatsData?cdCat01=A1101&cdTime={YEAR}100000&appId=&lang=E&statsDataId=0000020101&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0",
-"Population_ja":"http://api.e-stat.go.jp/rest/3.0/app/getSimpleStatsData?cdCat01=A1101&cdTime={YEAR}100000&appId=&lang=J&statsDataId=0000020101&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"
-}
+
+API_URLS: Final = dict(
+Population_en = "http://api.e-stat.go.jp/rest/3.0/app/getSimpleStatsData?cdCat01=A1101&appId=&lang=E&statsDataId=0000020101&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0",
+Population_ja = "http://api.e-stat.go.jp/rest/3.0/app/getSimpleStatsData?cdCat01=A1101&appId=&lang=J&statsDataId=0000020101&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"
+)
 """
 `API_URLS` are canned urls for API calls to e-Stat that are used in this module. They are coded with `typing.Final`_ to prevent redefinition. These particular calls download a table of total populations simply because the API cannot download the index column by itself.
 
 .. _typing.Final: https://docs.python.org/3/library/typing.html#typing.Final
 
+Canned urls
+-----------
+Population_en
+    Population table with municipal names in English
+Population_ja
+    Population table with municipal names in Japanese
+
+API_URLS : Final = dict(
+
+Population_en = `"http://api.e-stat.go.jp/rest/3.0/app/getSimpleStatsData?cdCat01=A1101
+&appId=&lang=E&statsDataId=0000020101
+&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y
+&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"`,
+
+Population_ja = `"http://api.e-stat.go.jp/rest/3.0/app/getSimpleStatsData?cdCat01=A1101
+&appId=&lang=J&statsDataId=0000020101
+&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y
+&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"`
+
+)
+
 """
 
+def municipal_code_download_csv(file = "../data/municipal-codes.csv"):
+    """Download Japanese and English municipal names and codes from [System of Social and Demographic Statistics A Population and Households](https://www.e-stat.go.jp/en/dbview?sid=0000020101) and construct a Japanese-English code dataframe stored in csv format.
+
+    The source dataset is updated every five years after each population census. As displayed at the e-Stat website, the table appears to contain annual survey data thereby requiring a filter to retrieve only census years. In the contrary, it contains data only for census years; omitting the year filter still retrieves data from only census years.
+
+
+    """
+    import datetime
+    from estatjp import api
+    from estatjp import exceptions as xs
+    import pandas as pd
+
+    # get urls
+    url_en = API_URLS.get('Population_en')
+    url_ja = API_URLS.get('Population_ja')
+
+    columns = ['census_year','muni_code','muni_name_en','muni_name_ja','population']
+    
+    # Get the codes with municipal names in English.
+    try:
+        dfs_en = api.get_csv_data(url_en,description=datetime.datetime.now())
+    except Exception as e:
+        print(type(e))
+        print(e.args)
+        print(e.with_traceback)
+
+    # Get the codes with municipal names in Japanese.
+    try:
+        dfs_ja = api.get_csv_data(url_ja,description=datetime.datetime.now())
+    except Exception as e:
+        print(type(e))
+        print(e.args)
+        print(e.with_traceback)
+
+    # Merge the results
+    main_en = dfs_en.get('Main')
+    main_en = main_en[["SURVEY YEAR","time_code","area_code","AREA","value"]]
+    main_ja = dfs_ja.get('Main')
+    main_ja = main_ja[["調査年","time_code","area_code","地域","value"]]
+    maindf = pd.merge(main_en,main_ja, on=["time_code","area_code","value"] )
+    df_result = maindf[["SURVEY YEAR","area_code","AREA","地域","value"]]
+    df_result.columns = columns
+
+    df_result.to_csv(file, index=False)
+
+    return df_result
